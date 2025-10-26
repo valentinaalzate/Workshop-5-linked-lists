d856068378d404ebf0b0c6307ff0c4b65b6b92ea
valentinaalzate <vaev39@gmail.com>
2025-10-26 17:01:03 -0500

    double linked lists

---
+++ LinkedList/DoubleList/DoubleNode.cs
@@ -0,0 +1,20 @@
+﻿namespace DoubleList
+{
+    public class DoubleNode<T>
+    {
+        public DoubleNode(T data)
+        {
+            Data = data;
+            Prev = null;
+            Next = null;
+        }
+
+        public DoubleNode<T>? Prev { get; set; }
+        public T? Data { get; set; }
+        public DoubleNode<T>? Next { get; set; }
+
+
+
+
+    }
+}
